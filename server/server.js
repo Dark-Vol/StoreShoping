56f099ff7bb8b9b36f81156f651a0b4ebce41e19
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-10-05 20:22:10 +0300

    update main file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
-const http = require('http')
-// const  = require('./routes/')
+const express = require('express')
+const cors = require('cors')
+const router = require('./routes')
 
 const ip = '127.0.0.1'
 const port = 3000
 
-const server = http.createServer()
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+// server.use('/api',router)
 
 server.listen(port, ip, () => console.log('server start'))
