351ad5651614f0891ce052acfd96064ebefbccee
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-10-07 14:12:21 +0300

    add new controllers

---
+++ server/controllers/categoryController.js
@@ -0,0 +1,13 @@
+class CategoryController {
+    static async getAll(req,resp) {
+        return resp.status(200).json({message:"GET ALL OK"})
+    }
+    
+    static async create(req,resp) {
+        console.log(req.body)
+        return resp.status(201).json({message:"CREATE OK"})
+    }
+}
+
+
+module.exports = CategoryController
