0e0b85f939ad096a7123841a87d5210e5b0e225c
Vavilov Nikita <67798734+Overlord228-png@users.noreply.github.com>
2024-10-07 14:15:10 +0300

    update main file server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const Router = require('express')
 const productsRouter = require("./productsRouter")
+const categoryRouter = require("./categoryRouter")
+const commentsRouter = require("./commentsRouter")
 
 const router = new Router()
 
-router.use('/posts', productsRouter)
+router.use('/category', categoryRouter)
+router.use('/products', productsRouter)
+router.use('/comments', commentsRouter)
 
 module.exports = router
