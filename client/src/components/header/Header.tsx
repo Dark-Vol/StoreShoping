a0af8e5bf5873f7539bde8a24e34d946b897d527
Dark-Vol <overlord28985@gmail.com>
2024-11-19 22:34:50 +0200

    fix bag

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo1 from "../../assets/img/logo1.svg"
+import logo2 from "../../assets/img/614589c0d6b3c0d2877ac97807dca434.png"
+import logo5 from "../../assets/img/d19fae149b706bea5cb2cff6d995a101.png"
 
 const Header: React.FC = () => {
   return (
@@ -8,7 +9,8 @@ const Header: React.FC = () => {
         <div className="header__conteiner">
           <div className="header__content-menu">
             <div className="header__content-menu-logo">
-              <img src={logo1} />
+              <img src={logo2} alt=''/>
+              <img src={logo5} alt=''/>
             </div>
           </div>
         </div>
@@ -16,5 +18,4 @@ const Header: React.FC = () => {
     </header>
   );
 }
-
 export default Header;
